08644adbd51edede79ec81e892745832711469ed
D Sharath kumar rao <dsharathrao@gmail.com>
2020-03-05 18:37:16 +0530

    simple website open code done

---
+++ mywipro.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+
+kala = webdriver.Chrome()
+kala.maximize_window()
+kala.implicitly_wait(20)
+kala.get('https://mywipro.wipro.com/')
+
+username = kala.find_element_by_xpath('//*[@id="i0116"]')
+username.send_keys('sharrath')	
