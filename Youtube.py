f9c81e407aaf09aedce7fd86fd66369584ccbb23
D Sharath kumar rao <dsharathrao@gmail.com>
2020-03-06 00:48:27 +0530

    Telugu movies searching automation added

--- Youtube.py
+++ Youtube.py
@@ -2,4 +2,12 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.maximize_window()
+driver.implicitly_wait(20)
 driver.get('https://www.youtube.com/')
+
+username = driver.find_element_by_xpath('//*[@id="search"]')
+username.send_keys('telugu movies')	
+#//*[@id="search-icon-legacy"]
+
+searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+searchButton.click()
