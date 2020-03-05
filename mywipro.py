2c87bdf232f62102112a47c59f090db8dd590da8
D Sharath kumar rao <dsharathrao@gmail.com>
2020-03-05 19:07:08 +0530

    replaced

--- mywipro.py
+++ mywipro.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 
-kala = webdriver.Chrome()
-kala.maximize_window()
-kala.implicitly_wait(20)
-kala.get('https://mywipro.wipro.com/')
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(20)
+driver.get('https://mywipro.wipro.com/')
 
-username = kala.find_element_by_xpath('//*[@id="i0116"]')
-username.send_keys('sharrath')	
+username = driver.find_element_by_xpath('//*[@id="i0116"]')
+username.send_keys('hhh')	
