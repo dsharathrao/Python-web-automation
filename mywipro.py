779d925035c0c0827fbcfe9bb32e733ce753bfb5
D Sharath Kumar Rao <50975866+dsharathrao@users.noreply.github.com>
2022-08-06 01:17:26 +0530

    Delete mywipro.py

--- mywipro.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.implicitly_wait(20)
-driver.get('https://mywipro.wipro.com/')
-
-username = driver.find_element_by_xpath('//*[@id="i0116"]')
-username.send_keys('hhh')	
